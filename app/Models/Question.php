fe4427222614e3b3ccd2e5a775b7fa944a48e407
gepopp <gerhard@poppgerhard.at>
2025-09-16 07:32:00 +0200

    make Questions slugable

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -3,12 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Question extends Model
 {
+    use HasSlug;
+
+
+
     public function section()
     {
         return $this->belongsTo(Section::class);
 
+    }
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('question')
+            ->saveSlugsTo('slug');
     }
 }
